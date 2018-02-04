83c6253c0d372abc5529fb68cef658698ad3494e
Chenjuan <18911620138@163.com>
2018-02-04 18:03:03 +0800

    添加业务模型(user部分)

---
+++ models/user.js
@@ -0,0 +1,37 @@
+
+
+const {query} = require('../utilities/db-helper.js')
+
+exports.findByEmail = (email,callback)=>{
+	const sql = 'SELECT * FROM `users` WHERE `email` = ?'
+	query(sql, [email],(err,results)=>{
+		if(err){
+			return callback(err)
+		}
+
+		callback(null,results[0])
+
+	})
+}
+
+exports.findByNickName = (nickName,callback)=>{
+	const sql = 'SELECT * FROM `users` WHERE `nickname`=?'
+	query(sql,[nickName],(err,results)=>{
+		if(err){
+				return callback(err)
+		}
+
+		callback(null,results[0])
+	})
+}
+
+exports.save = (user,callback) => {
+	const sql = 'INSERT INTO `users` SET ?'
+	query(sql,user,(err,result)=>{
+		if(err){
+			return callback(err)
+		}
+
+		callback(result)
+	})
+}
