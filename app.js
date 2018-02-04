1dd4c9bb5ea0e5a6f1e9b0d6ca1b03f161599c44
Chenjuan <18911620138@163.com>
2018-02-04 18:16:54 +0800

    配置 body-parser 中间件解析表单 POST 请求体

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
 const router = require('./router.js')
+const bodyParser = require('bodyParser')
+
+const app = express()
 
 
 // 挂在路由容器到app是路由生效
@@ -9,6 +11,8 @@ app.use(router)
 // 开放静态资源
 app.use('/node_modules',express.static('./node_modules/'))
 app.use('/public',express.static('./public/'))
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.engine('html',require('express-art-template'))
 
